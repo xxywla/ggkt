655d6a12693b5bf6c1657a824deef10868519b0c
qrt <xxywqrt@qq.com>
2022-10-26 22:48:15 +0800

    添加讲师头像

--- vue-admin-template/src/views/vod/teacher/form.vue
+++ vue-admin-template/src/views/vod/teacher/form.vue
@@ -30,7 +30,12 @@
       </el-form-item>
 
       <!-- 讲师头像 -->
-      <el-form-item label="讲师头像"> </el-form-item>
+      <el-form-item label="讲师头像">
+        <el-upload :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" :on-error="handleAvatarError" :action="BASE_API+'/admin/vod/file/upload'" class="avatar-uploader">
+          <img v-if="teacher.avatar" :src="teacher.avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon" />
+        </el-upload>
+      </el-form-item>
 
       <el-form-item>
         <el-button type="primary" @click="saveOrUpdate()">保存</el-button>
@@ -49,6 +54,7 @@ export default {
         sort: 0,
         level: 1,
       },
+      BASE_API: "http://localhost:8301",
     };
   },
   created() {
@@ -58,6 +64,38 @@ export default {
     }
   },
   methods: {
+    // 上传成功回调
+    handleAvatarSuccess(res, file) {
+      // console.log(res)
+      if (res.code == 20000) {
+        // console.log(res)
+        this.teacher.avatar = res.data;
+        // 强制重新渲染
+        this.$forceUpdate();
+      } else {
+        this.$message.error("上传失败 （非0）");
+      }
+    },
+
+    // 错误处理
+    handleAvatarError() {
+      console.log("error");
+      this.$message.error("上传失败（http失败）");
+    },
+
+    // 上传校验
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
     // 添加新的讲师
     save() {
       teacherApi.addTeacher(this.teacher).then((res) => {
